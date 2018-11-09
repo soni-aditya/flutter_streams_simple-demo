a6a93d41930a149e81b58e0e06eabe24a8059f0d
Aditya Soni <adityasoni5859@gmail.com>
2018-11-09 15:36:10 +0530

    added a simple infinite photo list app code using streams.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(new MyApp());
 
@@ -7,31 +11,98 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Photo Streamer',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: PhotoList(),
     );
   }
 }
 
-class Home extends StatefulWidget {
+class PhotoList extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _PhotoListState createState() => _PhotoListState();
 }
 
-class _HomeState extends State<Home> {
+class _PhotoListState extends State<PhotoList> {
+  StreamController<Photo> streamController;
+  List<Photo> photoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    //Initiating the stream controller
+    streamController = StreamController.broadcast();
+    //subscribing to the stream
+    streamController.stream.listen((photo) {
+      setState(() {
+        photoList.add(photo);
+      });
+    });
+    //Load all the data from the api
+    load(streamController);
+  }
+
+  load(StreamController sController) async {
+    String url = 'https://jsonplaceholder.typicode.com/photos';
+    var client = http.Client();
+    var request = http.Request('get', Uri.parse(url));
+    var streamResponse = await client.send(request);
+    streamResponse.stream
+        .transform(utf8.decoder)
+        .transform(json.decoder)
+        //Taking each photo from the list and changing it into a collection
+        .expand((singlePhotoFromList) => singlePhotoFromList)
+        .map((map) => Photo.mapFromJson(map))
+        .pipe(streamController);
+  }
+
+  @override
+  void dispose() {
+    streamController?.close();
+    streamController = null;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Streamer'),
+      ),
+      body: Center(
+        child: ListView.builder(
+            cacheExtent: 1500.0,
+            itemBuilder: (BuildContext context, int index) =>
+                _makeElement(index)),
+      ),
+    );
   }
+
+  _makeElement(int index) {
+    if (index >= photoList.length) {
+      return null;
+    }
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(photoList[index].url),
+          Text(photoList[index].title),
+        ],
+      ),
+    );
+  }
+}
+
+class Photo {
+  final String title;
+  final String url;
+
+  Photo(this.title, this.url);
+
+  Photo.mapFromJson(Map jsonMap)
+      : title = jsonMap['title'],
+        url = jsonMap['url'];
 }
